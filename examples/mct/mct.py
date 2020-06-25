6dc6891d66c30d9c6d28b0fea625a60eed6de427
Hugo Linder <hugoli@kth.se>
2020-06-25 06:58:35 +0200

    added Mamo's notebooks (from Shannon, copiemct)
    - demonstrates mCT usage
    
    - note, the data is on KTH NAS, see imagingnas > data > Users > olivierv
    
    - reads from Siemens sinograms

---
+++ examples/mct/mct.py
@@ -0,0 +1,19 @@
+from odlpet import Scanner
+
+class mCT(Scanner):
+    num_rings = 55
+    num_dets_per_ring = 624
+    voxel_size_xy = 4.0
+    default_non_arc_cor_bins = 312
+    intrinsic_tilt = 0.0
+    det_radius = 421.0
+    ring_spacing = 4.159999847412109
+    average_depth_of_inter = 7.0
+    max_num_non_arc_cor_bins = 312
+    axials_blocks_per_bucket = 1
+    trans_blocks_per_bucket = 1
+    axial_crystals_per_block = 55
+    trans_crystals_per_block = 13
+    axial_crystals_per_singles_unit = 55
+    trans_crystals_per_singles_unit = 13
+    num_detector_layers= 1
