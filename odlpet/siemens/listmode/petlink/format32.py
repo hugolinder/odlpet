eb826b63c45b2ae780aadc4d5fd6225c79f4e1d4
Hugo Linder <hugoli@kth.se>
2020-09-22 10:48:57 +0200

    ENH: improve packet classes names and construction, update format32.py to be compatible and demo PacketFormat

--- odlpet/siemens/listmode/petlink/format32.py
+++ odlpet/siemens/listmode/petlink/format32.py
@@ -1,66 +1,53 @@
-# parts and logic for 32 bit packets 
+# parts, packets and logic for 32 bit packets 
 
-class document_section:
-    def __init__(self, name, packets, pages = None):
-        self.packets = packets
-        self.name = name
-        self.pages = pages
-
-    def __repr__(self):
-        names = [self.name]
-        packet_names = "\n".join([packet.name for packet in self.packets])
-        context = "section({} )"
-        attributes= "name = {}\n, packets = \n{}".format(self.name, packet_names)
-        if self.pages is not None:
-            attributes = attributes + "\n, pages = {}".format(self.pages)
-                                
-        return context.format(attributes)
-
-
-
+from odlpet.siemens.listmode.petlink.packet import PacketFormat, PacketPart
+#
 #   PETLink documentation, start at page 4
 #   3.1 Overview, page 4
-EVENT = packet_part(31, 31, 0)
-TIME_TAG = packet_part(31-1, 31, 0b10)
-MOTION_TAG = packet_part(31-2, 31, 0b110)
+EVENT = PacketPart(31, 32, 0)
+TIME_TAG = PacketPart(31-1, 32, 0b10)
+MOTION_TAG = PacketPart(31-2, 32, 0b110)
 GANTRY_MOTION_POSITION_TAG = MOTION_TAG
-MONITORING_TAG = packet_part(31-3, 31, 0b1110)
-CONTROL_TAG = packet_part(31-3, 31, 0b1111)
+MONITORING_TAG = PacketPart(31-3, 32, 0b1110)
+CONTROL_TAG = PacketPart(31-3, 32, 0b1111)
 #   3.2 event packet
-PROMPT = packet_part(30, 30, 1)
-BIN_ADDRESS = packet_part(0, 29)
+PROMPT = PacketPart(30, 31, 1)
+DELAY = PacketPart(30, 31, 0)
+BIN_ADDRESS = PacketPart(0, 30)
+PROMPT_PACKET = PacketFormat(name = 'PET Bin-Address Event Packet (prompt)', parts = [EVENT, PROMPT, BIN_ADDRESS])
+DELAY_PACKET = PacketFormat(name = 'PET Bin-Address Event Packet (delay)', parts = [EVENT, DELAY, BIN_ADDRESS])
 #   skip SPECT event packet (obsolete)
 #   skip 4 maintenance packets, page 5
 #   3.2.1 time and dead time, page 6
-ELAPSED_TIME_MARKER = packet_part(31-2, 31, 0b100)
-TIME_MS = packet_part(0, 28) #seems to start at 0
-DEAD_TIME_MARKER = packet_part(31-2, 31, 0b101)
-DEAD_TIME_BLOCK = packet_part(19, 19+9)
-DEAD_TIME_SINGLES_RATE = packet_part(0, 18) #units vary
+ELAPSED_TIME_MARKER = PacketPart(31-2, 32, 0b100)
+TIME_MS = PacketPart(0, 29) #seems to start at 0
+DEAD_TIME_MARKER = PacketPart(31-2, 32, 0b101)
+DEAD_TIME_BLOCK = PacketPart(19, 19+10)
+DEAD_TIME_SINGLES_RATE = PacketPart(0, 19) #units vary
 #   skip expanded format, page 7
 #   3.2.2 Gantry motion and position, page 8
 #   skip 3 legacy packets 
 #       detector rotation (SPECT)
 #       head I "A" radial position
 #       head II "B" radial position
-IS_HORIZONTAL_BED = packet_part(31-7, 31, 0b11000100)
-HORIZONTAL_BED_POSITION = packet_part(0, 19) # units of 10 mikro-meter
-IS_HORIZONTAL_MOVING = packet_part(19+1, 19+1, 1)
-IS_VERTICAL_BED = packet_part(31-7, 31, 0b11000011) 
-VERTICAL_BED_POSITION = packet_part(0, 13) # unknown units
+IS_HORIZONTAL_BED = PacketPart(31-7, 32, 0b11000100)
+HORIZONTAL_BED_POSITION = PacketPart(0, 29) # units of 10 mikro-meter
+IS_HORIZONTAL_MOVING = PacketPart(19+1, 19+2, 1)
+IS_VERTICAL_BED = PacketPart(31-7, 32, 0b11000011) 
+VERTICAL_BED_POSITION = PacketPart(0, 14) # unknown units
 #   skip gantry Left/Right position
 #   skip source axial position and rotation, page 9
 #   skip HRRT single photon source position
 #   skip 3.2.3
-BASIC_GATING = packet_part(31-4, 31, 0b11100)
-BASIG_GATING_EXPANSION_FORMAT_CONTROL = packet_part(31-7,31-5)
-BASIC_GATING_E0_G = packet_part(0, 7)
-BASIC_GATING_E0_D = packet_part(0, 5) 
+BASIC_GATING = PacketPart(31-4, 32, 0b11100)
+BASIG_GATING_EXPANSION_FORMAT_CONTROL = PacketPart(31-7,31-4)
+BASIC_GATING_E0_G = PacketPart(0, 8)
+BASIC_GATING_E0_D = PacketPart(0, 6) 
 # Physiological Data (e.g. Respiratory Phase) Field: 0-5. 
 # D content only has validity if flag bit P=1
-BASIC_GATING_E0_P = packet_part(6) 
+BASIC_GATING_E0_P = PacketPart(6) 
 # Physiological (e.g. Respiratory) Flag Bit – High Tru
-BASIC_GATING_E0_C = packet_part(7) 
+BASIC_GATING_E0_C = PacketPart(7) 
 # Cardiac “R Wave” Flag Bit – High True. This bit is intended to be the primary trigger control 
 # for driving the automatic FPGA-driven Gating Buffer switching in designs such as the Smart DRAM.
 
--- odlpet/siemens/listmode/petlink/packet.py
+++ odlpet/siemens/listmode/petlink/packet.py
@@ -1,131 +1,69 @@
 #The general packet classes from the current petlink32.py
 #This provides some structure to the content of the PETLinkt guideline. 
 
-class packet:
-    def __init__(self, instruction, packet_name = None, part_names = None, is_twos_complement = None):
-        instruction_lines = instruction.split("\n")
-        if len(instruction_lines) > 1: # alternative input format
-            packet_name = instruction_lines[0]
-            instruction = instruction_lines[1]
-            part_names = instruction_lines[2:]
-        self.instruction = instruction
-        self.part_names = part_names
-        self.is_twos_complement = is_twos_complement
-        self.name = packet_name
-        instruction = instruction.replace(" ", "") 
-        self.len = len(instruction)
-        if self.len != 32:
-            print("warning, instruction length does not indicate 32 bit packet")
-            print("instruction {}, of length {}".format(instruction, self.len))
-        self.parts = parse_instruction(instruction) 
-        # finally, naming and twos_complement
-        if self.part_names is not None: 
-            for part, name in zip(self.parts, self.part_names):
-                part.name = name
-        if self.is_twos_complement is not None:
-            for is_signed, part in zip(self.is_twos_complement, self.parts):
-                part.is_twos_complement = is_signed
-
-    def __repr__(self):
-        parts_info = [part.__repr__() for part in self.parts]
-        parts_info = "\n---\n".join(parts_info)
-        return "packet(name = {}\ninstruction = {}\nparts = \n{}\n)".format(self.name, self.instruction, parts_info)
+class PacketFormat:
+    """Implementation of Packet format details from PETLINK guideline. 
+    For simplicity, packet formats have a partition of its bits into parts, corresponding to the fields in the guideline. 
+    Thus, the more complicated 64-bit format would be represented as several different PacketFormats, 
+    instead of one complicated format. 
+    A packet is simply a number."""
+    def __init__(self, description=None, name=None, partition=None, visual_guide=None, parts = None, comment=None, numbits=32):
+        self.description = description
+        #if self.description is not None: # To Do: parse description for automatic creation based on petlink text
+        self.name = name
+        self.partition = partition
+        self.visual_guide = visual_guide
+        self.parts = parts
+        self.comment = comment
+        self.numbits = numbits
         
-    def compare(self, number):
-        """ compares with all value parts, returns True when all value parts compare True. """
+    def compare(self, packet):
+        """ compares with all value fields, returns True when all value fields compare True. """
         result = True
         for part in self.parts:
             if part.value is not None:
-                result = part.compare(number) == result
+                result = part.compare(packet) == result
         return result
 
-    def evaluate(self, number):
-        """ return sequence of number evalations, one per part in the packet. """
-        return [part.evaluate(number) for part in self.parts]
+    def evaluate(self, packet):
+        """ return sequence of packet number evalations, one per field in the packet. """
+        return [part.evaluate(packet) for part in self.parts]
+    
+    def details(self):
+        return "{}({})".format(self.__class__.__name__, "**{}".format(str(self.__dict__)))
+    
+    def __repr__(self):
+        return "{}(name={})".format(self.__class__.__name__, self.name)
 
-class packet_part:
-    def __init__(self, lowbit, highbit=None, value=None, name=None, is_twos_complement = False):
+class PacketPart:
+    """A simpler version of the packet fields in the guideline. 
+    A part is a range into the packet bits, with some properties for evaluation.
+    A packet is simply a number.
+    """
+
+    def __init__(self, lowbit, highbit=None, value=None, name=None, description=None, is_twos_complement = False):
+        self.name = name
         self.lowbit = lowbit
         if highbit is None:
-            highbit = lowbit
+            highbit = lowbit + 1
         self.highbit = highbit
-        self.nbits = highbit - lowbit + 1
+        self.nbits = highbit - lowbit
         self.value = value
-        self.mask = get_mask(lowbit, highbit)
-        self.name = name
+        self.mask = ((1 << self.nbits) - 1) << lowbit
+        self.mask_hexadecimal = "{0:x}".format(self.mask)
+        #self.mask_nibbles = nibble_string(self.mask, self.nbits)
         self.is_twos_complement = is_twos_complement
-
-    def evaluate(self, number, is_twos_complement =None):
-        """ evaluate number, in mask, relative to lowbit. Perform sign extension if a twos complement value (default to self) """
-        if is_twos_complement is None:
-            is_twos_complement = self.is_twos_complement
-        value = (self.mask & number) >> self.lowbit
-        if is_twos_complement:
-            value = sign_extend(value, self.nbits)
-        return value
-
-    def compare(self, number):
-        return self.evaluate(number) == self.value
-
-    def __repr__(self):
-        names = []
-        if self.name is not None:
-            names.append(self.name)
-        names.append( "(lowbit, highbit, nbits) = {}".format( (self.lowbit, self.highbit, self.nbits) ) )
-        if self.value is not None:
-            names.append("value = {0:b} (base 2)".format(self.value))
+    
+    def evaluate(self, packet):
+        """Evaluate packet, in mask, relative to lowbit. Perform sign extension if a twos complement value """
+        value = (self.mask & packet) >> self.lowbit
         if self.is_twos_complement:
-            names.append("field is a twos complement number")
-        names.append("mask = {}".format(nibble_string(self.mask)))  
-        return "\n".join(names)
-
-def parse_instruction(instruction):
-    """ parse doc string, create parts"""
-    instruction = instruction.replace(" ", "")
-    instruction_parts = [] # first, split instruction into parts
-    part_string = ""
-    part_char = instruction[0]
-    for char in instruction:
-        if not (char == part_char): # that is, not AA, 00, 11, ...
-            pair = char + part_char
-            if not ((pair == "01") or (pair == "10")):
-                instruction_parts.append(part_string)
-                part_string = ""
-                part_char = char
-        part_string = part_string + char
-    instruction_parts.append(part_string)
-    parts = [] # secondly, create a packet part for each instruction part
-    offset = 0
-    for part in instruction_parts:
-        nbits = len(part)
-        offset = offset + nbits
-        lowbit = len(instruction) - offset
-        highbit = lowbit + (nbits-1)
-        part_char = part[0]
-        is_value = (part_char == '1' ) | (part_char == '0' )
-        value = int(part, base=2) if is_value else None
-        parts.append(packet_part(lowbit, highbit, value))
-    return parts
-
-def get_mask(lowbit, highbit):
-    """ 1s from lowbit (LSB) to highbit (MSB), 0 elsewhere. Example, get_mask(1,2) --> 110 (binary)  """
-    size = highbit + 1 - lowbit
-    mask = 1
-    mask = (mask << size) - 1
-    return mask << lowbit
-
-def nibble_string(number, nbits=32):
-    """ string representation of bits, with nibbles (4 bit chunks) separated by blank spaces """
-    number2 = number + (1 << (nbits)) # add a leading 1, so that leading 0's will be included
-    res = "{0:b}".format(number2)[1:]  # remove leading 1
-    offset = nbits % 4
-    pre_nibble = [res[:offset]]
-    size_nibbles = (nbits//4)
-    nibbles =  [res[(offset+4*n):(offset + 4*(n+1))] for n in range(size_nibbles)]
-    res = pre_nibble + nibbles
-    return " ".join(res)
- 
-def sign_extend(number, nbits):
-    """ convert a 2's complement natural number to the corresponding integer """
-    signbit = (number >> (nbits-1)) & 1
-    return signbit*(number - (1 << nbits)) + (signbit == 0)*number
+            signbit = (value >> (self.nbits-1)) & 1
+            value = signbit*(value - (1 << nbits)) + (signbit == 0)*value
+        return value
+    
+    def compare(self, packet):
+        return self.evaluate(packet) == self.value
+    
+    def __repr__(self):
+        return "{}({})".format(self.__class__.__name__, "**{}".format(str(self.__dict__)))
