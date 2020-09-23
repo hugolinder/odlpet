211cef32e47685b7b3eb8760c6aade2dd1ab82f5
lena.douglas <lena@savantic.se>
2020-09-23 10:33:23 +0200

    Updated format32.py for cardiac and respiratory gating.

--- odlpet/siemens/listmode/petlink/format32.py
+++ odlpet/siemens/listmode/petlink/format32.py
@@ -40,8 +40,10 @@ VERTICAL_BED_POSITION = PacketPart(0, 14) # unknown units
 #   skip HRRT single photon source position
 #   skip 3.2.3
 BASIC_GATING = PacketPart(32-5, 32, 0b11100)
-BASIG_GATING_EXPANSION_FORMAT_CONTROL = PacketPart(32-5-3,32-5, name = "E")
-BASIC_GATING_PACKET = PacketFormat(parts=[BASIC_GATING, PacketPart(32-5-3,32-5, name = "E"), PacketPart(0, 32-5-3, name = "E")])
+BASIC_GATING_EXPANSION_FORMAT_CONTROL = PacketPart(32-5-3,32-5, name = "E")
+BASIC_GATING_PACKET = PacketFormat(parts=[BASIC_GATING, BASIC_GATING_EXPANSION_FORMAT_CONTROL, PacketPart(0, 32-5-3, name = "u")])
+
+# Expanded Format #0 (Legacy), i.e. EEE=000
 BASIC_GATING_E0_G = PacketPart(0, 8)
 BASIC_GATING_E0_D = PacketPart(0, 6) 
 # Physiological Data (e.g. Respiratory Phase) Field: 0-5. 
@@ -52,5 +54,25 @@ BASIC_GATING_E0_C = PacketPart(7)
 # Cardiac “R Wave” Flag Bit – High True. This bit is intended to be the primary trigger control 
 # for driving the automatic FPGA-driven Gating Buffer switching in designs such as the Smart DRAM.
 
+# Expanded Format #1 (Gating), i.e. EEE=001
+BASIC_GATING_E1_G = PacketPart(0, 16) #Gating 0-15
+BASIC_GATING_E1_D = PacketPart(0, 10) #Physiological data field 0-9
+
+BASIC_GATING_E1_C = PacketPart(15) # Cardiac -  the intent is for this C bit to only be set = 1 for single ECG environments. For multiple ECG environments - e.g. MR-PET, see P=2 <== presumably not used for CT-PET
+
+BASIC_GATING_E1_P = PacketPart(15-3, 15) #Physiological Type Field
+
+# Physiological gating - cardiac
+GATING_PRIMARY_R_WAVE_TRIGGER = PacketFormat(parts = [PacketPart(32-5-3,32-5, value=1, name = "E"), PacketPart(15-3, 15, value=2, name="P"), PacketPart(0, 10, value=0, name="D")]) #  "primary" R Wave Trigger - e.g. MR-supplied ECG
+GATING_SECONDARY_R_WAVE_TRIGGER = PacketFormat(parts = [PacketPart(32-5-3,32-5, value=1, name = "E"), PacketPart(15-3, 15, value=2, name="P"), PacketPart(0, 10, value=1, name="D")]) # "secondary" R Wave Trigger - e.g. MR-supplied Oxygen Probe 
+GATING_TERTIARY_R_WAVE_TRIGGER = PacketFormat(parts = [PacketPart(32-5-3,32-5, value=1, name = "E"), PacketPart(15-3, 15, value=2, name="P"), PacketPart(0, 10, value=2, name="D")]) # "tertiary" R Wave Trigger - e.g. customer-supplied ECG
+GATING_CARDIAC_PHASE_WAVEFORM_CONTENT = PacketFormat(parts = [PacketPart(32-5-3,32-5, value=1, name = "E"), PacketPart(15-3, 15, value=6, name="P")]) # P = 6 indicates Cardiac Phase/Waveform Content in D
+
+# Physiological gating - respiratory
+GATING_RESPIRATORY_TRIGGER_GATE_ON = PacketFormat(parts = [PacketPart(32-5-3,32-5, value=1, name = "E"), PacketPart(15-3, 15, value=1, name="P"), PacketPart(0, 10, value=0, name="D")]) # P = 1 indicates Respiratory Trigger Content in D. [For mMR: D=0 for Gate On...]
+GATING_RESPIRATORY_TRIGGER_GATE_OFF = PacketFormat(parts = [PacketPart(32-5-3,32-5, value=1, name = "E"), PacketPart(15-3, 15, value=1, name="P"), PacketPart(0, 10, value=1, name="D")]) # P = 1 indicates Respiratory Trigger Content in D. [For mMR: ... D=1 for Gate Off.]
+GATING_RESPIRATORY_PHASE_WAVEFORM_CONTENT = PacketFormat(parts = [PacketPart(32-5-3,32-5, value=1, name = "E"), PacketPart(15-3, 15, value=5, name="P")]) # P = 5 indicates Respiratory Phase/Waveform Content in D
+
+#   skip Expanded Format #2 (Generic Trigger)
 #   skip 3.2.4 page 12
 
